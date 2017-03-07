16da916ee9987d0cff5e8ca0b2dd7374fc39b8a2
Varga Zoltán <waress76@gmail.com>
2017-03-07 12:02:49 +0100

    Add the add function

---
+++ main.js
@@ -0,0 +1,9 @@
+var num1 = document.querySelector('#num1');
+var num2 = document.querySelector('#num2');
+var szum = document.querySelector('#szum');
+
+num1.addEventListener("input", add);
+
+function add() {
+  
+}
