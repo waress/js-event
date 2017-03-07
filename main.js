3f82dff9533f298299e43f832e55e777b12861ae
Varga Zoltán <waress76@gmail.com>
2017-03-07 12:47:52 +0100

    complete add function

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var num2 = document.querySelector('#num2');
 var szum = document.querySelector('#szum');
 
 num1.addEventListener("input", add);
+num2.addEventListener("input", add);
 
 function add() {
-  
+  var n1 = parseFloat(num1.value) || 0;
+  var n2 = parseFloat(num2.value) || 0;
+  szum.innerHTML = 'Két szám összege: ' + (n1 + n2);
 }
